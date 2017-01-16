e826eee907337d7fc465603dbf9c3159aaea9ec1
AndreySemechkov <andrey.semechkov@gmail.com>
2017-01-16 12:07:04 +0200

    finished buffer class

--- src/part3/buffer.cpp
+++ src/part3/buffer.cpp
@@ -1,19 +1,44 @@
 /*
- * buffer.cpp
+ * buffer functions for code generation
  *
- *  Created on: Jan 15, 2017
- *      Author: compm
+ *
  */
 
 #include "buffer.hpp"
 
+Buffer::Buffer(){
+	bufferLines = new vector<string>;
+}
 
-/* TODO:
-Buffer::Buffer(){}
-Buffer::~Buffer(){}
+Buffer::~Buffer(){
+	delete bufferLines;
+}
 
-void Buffer::emit(const string& command){}
-int Buffer::nextQuad(){}
-void Buffer::print(){}
-void Buffer::backpatch(vector<int>& backpatch_list , int lineNum){}
-*/
+//Adds another line of code to the buffer
+void Buffer::emit(const string& command){
+    bufferLines.push_back(command);
+}
+// Performs the backpatching action from the lines numbers counted from zero
+// input: topatch_list vector of line numbers that need patching
+void Buffer::backpatch(vector<int> lineNums , int address){
+	string addressString = to_string(address);
+	for (auto num : lineNums) {
+        bufferLines[num] += addressString;
+	}
+}
+
+// return: buffer size
+unsigned int Buffer::getSize(){
+	return bufferLines.size();
+}
+
+//prints all buffer lines
+void Buffer::print(){
+	for (auto line : bufferLines) {
+	       cout << line << endl;
+	}
+}
+
+
+
+
--- src/part3/buffer.hpp
+++ src/part3/buffer.hpp
@@ -1,7 +1,7 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-#include <string>
+#include "common.hpp"
 #include <vector>
 
 extern Buffer buffer;
@@ -10,17 +10,26 @@ using namespace std;
 
 class Buffer{
 
-	vector<string> quadList;
+private:
+	vector<string> bufferLines;
+
 public:
 
 	Buffer();
 	~Buffer();
 
+	//Adds another line of code to the buffer
 	void emit(const string& command);
-	int nextQuad();
 
+	// Performs the backpathing action from the lines numbers counted from zero
+	// input: topatch_list vector of line numbers that need patching
+	//        address data to attach
+	void Buffer::backpatch(vector<int> lineNums , int address){}
+
+	//prints all buffer lines
 	void print();
-	void backpatch(vector<int>& backpatch_list , int lineNum);
+	// return: buffer size
+	unsigned int getSize();
 };
 
 
