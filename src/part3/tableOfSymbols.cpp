af4343196e0a177ca3b5a71bef708f441a405b30
AndreySemechkov <andrey.semechkov@gmail.com>
2017-01-17 10:53:26 +0200

    tableOfSymbols class created, new test,

---
+++ src/part3/tableOfSymbols.cpp
@@ -0,0 +1,22 @@
+/*
+ * tableOfSymbols.cpp
+ *
+ *  Created on: Jan 16, 2017
+ *      Author: Andrey
+ */
+
+#include "tableOfSymbols.hpp"
+
+namespace std {
+
+TableOfSymbols::TableOfSymbols() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TableOfSymbols::~TableOfSymbols() {
+	// TODO Auto-generated destructor stub
+}
+
+
+} /* namespace std */
