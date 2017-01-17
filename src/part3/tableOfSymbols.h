6ebf83b8437efca5fdfeb85a0c97942ce627ba4c
AndreySemechkov <andrey.semechkov@gmail.com>
2017-01-17 11:02:04 +0200

    reverted my unwanted changes, and put back the wanted changes

---
+++ src/part3/tableOfSymbols.h
@@ -0,0 +1,23 @@
+/*
+ * tableOfSymbols.hpp
+ *
+ *  Created on: Jan 16, 2017
+ *      Author: Andrey
+ */
+
+#ifndef SRC_PART3_TABLEOFSYMBOLS_HPP_
+#define SRC_PART3_TABLEOFSYMBOLS_HPP_
+
+namespace std {
+
+class TableOfSymbols {
+private:
+
+public:
+	TableOfSymbols();
+	~TableOfSymbols();
+};
+
+} /* namespace std */
+
+#endif /* SRC_PART3_TABLEOFSYMBOLS_HPP_ */
