14da598940849ad59fafa6a41dcadda09277aaab
holylandman <saareliad@campus.technion.ac.il>
2017-01-23 11:43:41 +0200

    updated ver

--- src/part3/definitions.h
+++ src/part3/definitions.h
@@ -11,8 +11,8 @@
 // TYPES
 typedef enum { REAL, INT, NONE } t_type ;
 
-#define NUM_STACK_REGISTERS 4
-#define NUM_RETURN_REGISTERS 1
+#define NUM_STACK_REGISTERS 6 //I1, I2 - fp, I3,I4 - sp,  + I5,I6 - for save
+#define NUM_RETURN_REGISTERS 1 //I0
 
 #define EXIT_LEXICAL_FAILURE 1
 #define EXIT_SYNTAX_FAILURE 2
